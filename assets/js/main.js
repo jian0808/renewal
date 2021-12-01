38a63f9ff680c45da226c70bfe2bc5291a004816
jian0808 <90013630+jian0808@users.noreply.github.com>
2021-12-02 02:03:18 +0900

    20211202

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,13 @@
 
 // main slide Swiper
   var swiper = new Swiper(".mySwiper", {
-    cssMode: true,
-    loop: true,
-    // autoplay: {
-    //     delay: 2500,
-    //     disableOnInteraction: false,
-    // },
+    // cssMode: true,
+    loop: true,    
+    grabCursor: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
@@ -15,7 +16,7 @@
       el: ".swiper-pagination",
       clickable: true,
     },
-    mousewheel: true,
+    // mousewheel: true,
     keyboard: true,
   });
 
